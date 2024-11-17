6075ee5fda4194cd5719bcc4b4e823e55b2aa1a9
PedroHFG <pedrohfidg@gmail.com>
2024-11-17 12:58:34 -0300

    Database seeding

--- src/main/java/com/devsuperior/desafio2/entities/Atividade.java
+++ src/main/java/com/devsuperior/desafio2/entities/Atividade.java
@@ -16,7 +16,7 @@ public class Atividade {
     private Double preco;
 
     @ManyToOne
-    @JoinColumn(name = "catgoria_id")
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
     @OneToMany(mappedBy = "atividade")
